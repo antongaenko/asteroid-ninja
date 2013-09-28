482f11060e78ee611f33cf03a2a99330a5e66e95
Anton Gaenko <j.stb.ue@gmail.com>
2013-09-28 10:07:50 +0400

    fix test

--- ios/Asteroid NinjaTests/Math2DTest.mm
+++ ios/Asteroid NinjaTests/Math2DTest.mm
@@ -14,8 +14,6 @@ using namespace math2d;
 // TODO Write cross-platfrom tests
 @implementation Math2DTest
 
-Matrix2 *one;
-Matrix2 *two;
 Vector *v1;
 Vector *v2;
 Matrix *m1;
@@ -24,8 +22,6 @@ Matrix *m2;
 
 - (void)setUp {
   [super setUp];
-  one = new Matrix2();
-  two = new Matrix2();
   v1 = new Vector(10, -10, 1);
   v2 = new Vector(5, -5, 1);
   m1 = new Matrix(true);
@@ -34,81 +30,10 @@ Matrix *m2;
 
 - (void)tearDown {
   [super tearDown];
-  delete one;
-  delete two;
   delete v1;
   delete v2;
   delete m1;
   delete m2;
-}
-
-// Empty matrix (with all 0)
-- (void)testEmptyMatrix2 {
-  STAssertEquals((*one)[0][0], 0.0f, nil);
-  STAssertEquals((*one)[0][1], 0.0f, nil);
-  STAssertEquals((*one)[1][0], 0.0f, nil);
-  STAssertEquals((*one)[1][1], 0.0f, nil);
-}
-
-// Identity matrix should have 1 for diagonal
-- (void)testIdentityMatrix2 {
-  one->setIdentity();
-  STAssertEquals((*one)[0][0], 1.0f, nil);
-  STAssertEquals((*one)[0][1], 0.0f, nil);
-  STAssertEquals((*one)[1][0], 0.0f, nil);
-  STAssertEquals((*one)[1][1], 1.0f, nil);
-}
-
-// Nil (with all 0) * Identity = Nil
-- (void)testMultNil {
-  one->setIdentity();
-  Matrix2 r = *one * *two;
-  STAssertEquals(r[0][0], 0.0f, nil);
-  STAssertEquals(r[0][1], 0.0f, nil);
-  STAssertEquals(r[1][0], 0.0f, nil);
-  STAssertEquals(r[1][1], 0.0f, nil);
-}
-
-// Identity * Identity = Identity
-- (void)testMultIdentities {
-  one->setIdentity();
-  two->setIdentity();
-  Matrix2 r = *one * *two;
-  STAssertEquals(r[0][0], 1.0f, nil);
-  STAssertEquals(r[0][1], 0.0f, nil);
-  STAssertEquals(r[1][0], 0.0f, nil);
-  STAssertEquals(r[1][1], 1.0f, nil);
-}
-
-// Identity * Not Nil = Not Nil
-- (void)testMultIdentityAndNotNil {
-  one->setIdentity();
-  (*two)[0][0] = 1;
-  (*two)[0][1] = 2;
-  (*two)[1][0] = 3;
-  (*two)[1][1] = 4;
-  Matrix2 r = *one * *two;
-  STAssertEquals(r[0][0], 1.0f, nil);
-  STAssertEquals(r[0][1], 2.0f, nil);
-  STAssertEquals(r[1][0], 3.0f, nil);
-  STAssertEquals(r[1][1], 4.0f, nil);
-}
-
-// Not Nil  * Not Nil = Not Nil
-- (void)testMultNotNilAndNotNil {
-  (*one)[0][0] = 1;
-  (*one)[0][1] = 2;
-  (*one)[1][0] = 3;
-  (*one)[1][1] = 4;
-  (*two)[0][0] = 1;
-  (*two)[0][1] = 2;
-  (*two)[1][0] = 3;
-  (*two)[1][1] = 4;
-  Matrix2 r = *one * *two;
-  STAssertEquals(r[0][0], 7.0f, nil);
-  STAssertEquals(r[0][1], 10.0f, nil);
-  STAssertEquals(r[1][0], 15.0f, nil);
-  STAssertEquals(r[1][1], 22.0f, nil);
 }
 
 // Test Vector3
